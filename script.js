513d1837c7e945ca1419b41f0f421f5136aa73a9
mehak-garg <mehakgarg741@gmail.com>
2026-04-02 14:10:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,17 +6,21 @@ let select3=document.querySelector("input");
 
 select3.addEventListener("click",remove_color);
 function remove_color(){
-var selectedText = select2.options[select2.selectedIndex].text;
+	let colorSelect = document.getElementById("colorSelect");
+
+	colorSelect.remove(colorSelect.selectedIndex); 
+}	
+// var selectedText = select2.options[select2.selectedIndex].text;
 	
-	if(selectedText==="Black")
-{
-select2.innerHTML="<option>Red</option> <option>Green</option> <option>White</option>";
-}
-if(selectedText==="Red"){
-select2.innerHTML="<option>Green</option> <option>White</option> <option>Black</option> ";
-}
-if(selectedText==="White"){
-	select2.innerHTML="<option>Red</option> <option>Green</option> <option>Black</option> ";}
-if(selectedText==="Green"){
-select2.innerHTML="<option>Red</option> <option>White</option> <option>Black</option> ";
-}}
+// 	if(selectedText==="Black")
+// {
+// select2.innerHTML="<option>Red</option> <option>Green</option> <option>White</option>";
+// }
+// if(selectedText==="Red"){
+// select2.innerHTML="<option>Green</option> <option>White</option> <option>Black</option> ";
+// }
+// if(selectedText==="White"){
+// 	select2.innerHTML="<option>Red</option> <option>Green</option> <option>Black</option> ";}
+// if(selectedText==="Green"){
+// select2.innerHTML="<option>Red</option> <option>White</option> <option>Black</option> ";
+// }
