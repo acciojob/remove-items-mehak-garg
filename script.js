6beafcee8e30c2152530c84c934ba61c5df7f9e4
mehak-garg <mehakgarg741@gmail.com>
2026-04-02 14:00:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,23 +3,20 @@ let select1=document.querySelectorAll("option");
 let select2=document.querySelector("select");
 let select3=document.querySelector("input");
 
-console.log("select 1 ===> ", select1);
-console.log("select 2 ===> ", select2);
-console.log("select 3 ===> ", select3);
 
 select3.addEventListener("click",remove_color);
 function remove_color(){
 var selectedText = select2.options[select2.selectedIndex].text;
 	
-	if(selectedText==="black")
+	if(selectedText==="Black")
 {
 select2.innerHTML="<option>Red</option> <option>Green</option> <option>White</option>";
 }
-if(selectedText==="red"){
+if(selectedText==="Red"){
 select2.innerHTML="<option>Green</option> <option>White</option> <option>Black</option> ";
 }
-if(selectedText==="white"){
+if(selectedText==="White"){
 	select2.innerHTML="<option>Red</option> <option>Green</option> <option>Black</option> ";}
-if(selectedText==="green"){
+if(selectedText==="Green"){
 select2.innerHTML="<option>Red</option> <option>White</option> <option>Black</option> ";
 }}
